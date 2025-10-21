cd5c46f3ff7f035ffe21f73b6da729bfdf33fad3
高野拓真 <kumazo@takanotakushinnoMacBook-Pro.local>
2025-10-21 20:21:33 +0900

    libft.h modified

--- libft/libft.h
+++ libft/libft.h
@@ -7,6 +7,6 @@ void	*ft_memset(void *b, int c, unsigned int len);
 void	ft_bzero(void *s, unsigned int n);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
-size_t	ft_strlen(char *s)
+size_t	ft_strlen(char *s);
 
 #endif
