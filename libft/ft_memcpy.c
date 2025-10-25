8d98cbc61f122541e74e75fc41a57014ab61dde8
高野拓真 <kumazo@takanotakushinnoMacBook-Pro.local>
2025-10-25 17:30:16 +0900

    ft_memcpy modified

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tkono <tkono@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 15:49:31 by tkono             #+#    #+#             */
-/*   Updated: 2025/10/25 15:24:31 by tkono            ###   ########.fr       */
+/*   Updated: 2025/10/25 17:29:57 by tkono            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,13 @@ void	*ft_memcpy(void *dst, const void *src, unsigned int n)
 {
 	char			*s_dst;
 	char			*s_src;
+	unsigned int	i;
 
 	if (dst == NULL || src == NULL)
 		return (NULL);
 	s_dst = (char *)dst;
 	s_src = (char *)src;
+	i = 0;
 	while (i < n)
 	{
 		*(s_dst + i) = *(s_src + i);
